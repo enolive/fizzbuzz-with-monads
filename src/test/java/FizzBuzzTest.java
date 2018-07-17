17b0d0b5a8fad9dae1d0a269d522c1a07de13139
Christoph Welcz <christoph@welcz.de>
2018-07-18 00:46:30 +0200

    happy path sequence tests

--- src/test/java/FizzBuzzTest.java
+++ src/test/java/FizzBuzzTest.java
@@ -20,32 +20,38 @@ class FizzBuzzTest {
     @ParameterizedTest
     @ValueSource(ints = {1, 2, 4})
     void normal_numbers_should_be_returned_as_is(int input) {
-        assertThat(FizzBuzz.calculate(input).right().get()).isEqualTo(String.valueOf(input));
+        assertThat(FizzBuzz.calculate(input).get()).isEqualTo(String.valueOf(input));
     }
 
     @ParameterizedTest
     @ValueSource(ints = {3, 6, 9})
     void numbers_divisible_by_3_should_be_returned_as_fizz(int input) {
-        assertThat(FizzBuzz.calculate(input).right().get()).isEqualTo("Fizz");
+        assertThat(FizzBuzz.calculate(input).get()).isEqualTo("Fizz");
     }
 
     @ParameterizedTest
     @ValueSource(ints = {5, 10, 20})
     void numbers_divisible_by_5_should_be_returned_as_buzz(int input) {
-        assertThat(FizzBuzz.calculate(input).right().get()).isEqualTo("Buzz");
+        assertThat(FizzBuzz.calculate(input).get()).isEqualTo("Buzz");
     }
 
     @ParameterizedTest
     @ValueSource(ints = {15, 30, 45})
     void numbers_divisible_by_3_and_5_should_be_returned_as_fizz_buzz(int input) {
-        assertThat(FizzBuzz.calculate(input).right().get()).isEqualTo("Fizz-Buzz");
+        assertThat(FizzBuzz.calculate(input).get()).isEqualTo("Fizz-Buzz");
     }
 
     @Test
     void sequence_should_contain_the_expected_items() {
-        assertThat(FizzBuzz.sequence(15).right().get()).containsExactly(
+        assertThat(FizzBuzz.sequence(15).get()).containsExactly(
                 "1", "2", "Fizz", "4", "Buzz", "Fizz", "7", "8", "Fizz",
                 "Buzz", "11", "Fizz", "13", "14", "Fizz-Buzz"
         );
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {0, -1, -10000})
+    void sequence_with_limit_too_low_should_be_empty(int limit) {
+        assertThat(FizzBuzz.sequence(limit).get()).isEmpty();
     }
 }
