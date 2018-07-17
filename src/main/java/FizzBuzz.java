8712648685439d6566d0ecd99a71a288d7cd030a
Christoph Welcz <christoph@welcz.de>
2018-07-17 23:02:50 +0200

    some cleanup

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -5,12 +5,11 @@ import io.vavr.control.Option;
 
 import java.util.function.Predicate;
 
-import static io.vavr.API.Some;
 import static io.vavr.API.Tuple;
 
 class FizzBuzz {
 
-    public static final List<Tuple2<Predicate<Integer>, String>> RULES = List.of(
+    private static final List<Tuple2<Predicate<Integer>, String>> RULES = List.of(
             Tuple(isDivisibleBy(3), "Fizz"),
             Tuple(isDivisibleBy(5), "Buzz")
     );
@@ -27,11 +26,15 @@ class FizzBuzz {
     }
 
     private static String calculateRight(int input) {
-        return Some(RULES.filter(rule -> rule._1.test(input))
-                         .map(rule -> rule._2)
-                         .mkString("-"))
-                .filter(result -> !result.isEmpty())
-                .getOrElse(() -> String.valueOf(input));
+        return Option.some(getResultFromRules(input))
+                     .filter(result -> !result.isEmpty())
+                     .getOrElse(() -> String.valueOf(input));
+    }
+
+    private static String getResultFromRules(int input) {
+        return RULES.filter(rule -> rule._1.test(input))
+                    .map(rule -> rule._2)
+                    .mkString("-");
     }
 
     private static Predicate<Integer> isDivisibleBy(int divisor) {
