146796e0e03edd2d296a2d29f50f5364e112d7d2
Christoph Welcz <christoph@welcz.de>
2018-07-17 22:32:12 +0200

    some renaming

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -4,13 +4,13 @@ import io.vavr.control.Option;
 class FizzBuzz {
     static Either<String, String> calculate(int input) {
         return Option.of(input)
-                     .filter(FizzBuzz::isValid)
+                     .filter(FizzBuzz::validInput)
                      .map(FizzBuzz::calculateRight)
                      .toEither("Input should be positive");
     }
 
-    private static boolean isValid(int i) {
-        return i > 0;
+    private static boolean validInput(int input) {
+        return input > 0;
     }
 
     private static String calculateRight(int input) {
