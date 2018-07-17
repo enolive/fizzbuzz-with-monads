46db5b3a5cc819e6d9d64544b7ad8a8318bdc7f8
Christoph Welcz <christoph@welcz.de>
2018-07-17 22:18:42 +0200

    implemented first error check

--- src/test/java/FizzBuzzTest.java
+++ src/test/java/FizzBuzzTest.java
@@ -1,10 +1,17 @@
-import org.junit.jupiter.api.Test;
+import io.vavr.control.Either;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class FizzBuzzTest {
-    @Test
-    void name() {
-        assertThat(true).isFalse();
+class FizzBuzzTest {
+    @ParameterizedTest
+    @ValueSource(ints = {0, -1, -1234})
+    void invalid_input_should_produce_an_error(int input) {
+        assertThat(calculate(input).left().get()).isEqualTo("Input should be positive");
+    }
+
+    private Either<String, String> calculate(int input) {
+        return Either.left("Input should be positive");
     }
 }
