3af54dae0020cb8099b7ab38bd4e3eba1e8090ff
Christoph Welcz <christoph@welcz.de>
2018-07-17 22:10:37 +0200

    initial setup with gradle and junit5

---
+++ src/test/java/FizzBuzzTest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class FizzBuzzTest {
+    @Test
+    void name() {
+        assertThat(true).isFalse();
+    }
+}
