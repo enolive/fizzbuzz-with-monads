569c9fa13fe97938e509f84413aabbafdd3be0dc
Christoph Welcz <christoph@welcz.de>
2018-07-17 22:59:36 +0200

    using list instead of pattern matching to get rid of the duplication

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -1,12 +1,20 @@
-import io.vavr.Predicates;
+import io.vavr.Tuple2;
+import io.vavr.collection.List;
 import io.vavr.control.Either;
 import io.vavr.control.Option;
 
 import java.util.function.Predicate;
 
-import static io.vavr.API.*;
+import static io.vavr.API.Some;
+import static io.vavr.API.Tuple;
 
 class FizzBuzz {
+
+    public static final List<Tuple2<Predicate<Integer>, String>> RULES = List.of(
+            Tuple(isDivisibleBy(3), "Fizz"),
+            Tuple(isDivisibleBy(5), "Buzz")
+    );
+
     static Either<String, String> calculate(int input) {
         return Option.of(input)
                      .filter(FizzBuzz::validInput)
@@ -19,12 +27,10 @@ class FizzBuzz {
     }
 
     private static String calculateRight(int input) {
-        return Match(input)
-                .option(
-                        Case($(Predicates.allOf(isDivisibleBy(3), isDivisibleBy(5))), "Fizz-Buzz"),
-                        Case($(isDivisibleBy(3)), "Fizz"),
-                        Case($(isDivisibleBy(5)), "Buzz")
-                )
+        return Some(RULES.filter(rule -> rule._1.test(input))
+                         .map(rule -> rule._2)
+                         .mkString("-"))
+                .filter(result -> !result.isEmpty())
                 .getOrElse(() -> String.valueOf(input));
     }
 
