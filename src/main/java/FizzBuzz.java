82c665037adac7c5ba9a34f9ea4feb37c8b4238b
Christoph Welcz <christoph@welcz.de>
2018-07-18 22:08:41 +0200

    made tests more generic

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -2,6 +2,6 @@ import io.vavr.control.Either;
 
 class FizzBuzz {
     static Either<String, String> convert(int input) {
-        return Either.right("1");
+        return Either.right(String.valueOf(input));
     }
 }
--- src/test/java/FizzBuzzTest.java
+++ src/test/java/FizzBuzzTest.java
@@ -1,11 +1,13 @@
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class FizzBuzzTest {
-    @Test
-    void normalNumbers() {
-        assertThat(FizzBuzz.convert(1).get()).isEqualTo("1");
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 4})
+    void normalNumbers(int input) {
+        assertThat(FizzBuzz.convert(input).get()).isEqualTo(String.valueOf(input));
     }
 
 }
