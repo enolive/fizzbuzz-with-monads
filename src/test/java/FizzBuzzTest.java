c81e3de5e5dcd0a6391736827a05fd1f23cc2af2
Christoph Welcz <christoph@welcz.de>
2018-07-18 22:06:40 +0200

    made test easier

--- src/test/java/FizzBuzzTest.java
+++ src/test/java/FizzBuzzTest.java
@@ -5,8 +5,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 class FizzBuzzTest {
     @Test
     void normalNumbers() {
-        final String result = FizzBuzz.convert(1).get();
-        assertThat(result).isEqualTo("1");
+        assertThat(FizzBuzz.convert(1).get()).isEqualTo("1");
     }
 
 }
