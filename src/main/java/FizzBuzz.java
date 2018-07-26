6a7c0fd7c5cab0603690abdb720b959b7e0fbff5
Christoph Welcz <christoph@welcz.de>
2018-07-26 18:45:36 +0200

    some refactorings

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -1,5 +1,3 @@
-import io.vavr.Tuple;
-import io.vavr.Tuple2;
 import io.vavr.collection.List;
 import io.vavr.control.Either;
 
@@ -13,17 +11,10 @@ class FizzBuzz {
                 new Rule(3, "Fizz"),
                 new Rule(5, "Buzz")
         );
-        return rules.filter(rule -> appliesTo(rule, input))
-                    .map(result -> result.result)
+        return rules.filter(rule -> rule.appliesTo(input))
+                    .map(Rule::getResult)
                     .headOption()
                     .getOrElse(() -> String.valueOf(input));
     }
 
-    private boolean appliesTo(Rule rule, int input) {
-        return isDivisibleBy(input, rule.divisor);
-    }
-
-    private boolean isDivisibleBy(int input, int divisor) {
-        return input % divisor == 0;
-    }
 }
--- src/main/java/Rule.java
+++ src/main/java/Rule.java
@@ -1,9 +1,17 @@
 class Rule {
-    final int divisor;
-    final String result;
+    private final int divisor;
+    private final String result;
 
     Rule(int divisor, String result) {
         this.divisor = divisor;
         this.result = result;
+    }
+
+    String getResult() {
+        return result;
+    }
+
+    boolean appliesTo(int input) {
+        return input % divisor == 0;
     }
 }
